ff9b80c3298507c095fe641587a3f00a2067bd4b
Amir Shojae <amirshojae@alphafx.co.uk>
2021-03-01 01:25:06 +0000

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This has just been updated
         </a>
       </header>
     </div>
