1a7ea76e66c424df2a42564ca0693d57e00b7a96
Amir Shojae <amirshojae@alphafx.co.uk>
2021-03-22 00:35:02 +0000

    fixed no use before define issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,8 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    // note you must disable the base rule as it can report incorrect errors
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
   },
 };
